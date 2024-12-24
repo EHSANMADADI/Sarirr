7978676a3007050bfb612d7307b73ba3557e2ab1
ehsan <ehsanmadadialengareqhi@gmail.com>
2024-12-24 22:37:22 +0330

    add api to speech

--- src/Componnent/SpeechEnhancement/Body.tsx
+++ src/Componnent/SpeechEnhancement/Body.tsx
@@ -8,25 +8,45 @@ import VoiceRecorder from '../Share/VoiceRecorder';
 import WavesurferPlayer from "@wavesurfer/react";
 import { FaRegCirclePlay } from "react-icons/fa6";
 import { FaPauseCircle } from "react-icons/fa";
+import { toast, ToastContainer } from 'react-toastify';
+import { useStore } from '../../Store/Store';
+import api from '../../Config/api';
+import loader from "../../IMG/tail-spin.svg";
 export default function Body() {
-  const [savedRecordings, setSavedRecordings] = useState(() => {
-    const storage = JSON.parse(localStorage.getItem("Speech") || "[]");
-    return storage;
-  });
+  const { audioURLs, removeRecording } = useStore();
+ const [savedRecordings, setSavedRecordings] = useState(audioURLs);
+  const [converting, setConverting] = useState<boolean[]>([]);
+  const [sucsessFullConverting, setSucsessFullConverting] = useState<boolean[]>(
+    []
+  );
   const [file, setFile] = useState<File | null>(null);
   const [isOpen, setIsOpen] = useState(false);
-  const [selectedLanguages, setSelectedLanguages] = useState("GAGNET");
-  const listLanguage = ["GAGNET", "DBAIAT"];
+  const [selectedModel, setSelectedModel] = useState("GAGNET");
+  const listModel = ["GAGNET", "DBAIAT"];
+    useEffect(() => {
+      setConverting(new Array(savedRecordings.length).fill(false));
+      setSucsessFullConverting(new Array(savedRecordings.length).fill(false));
+     
+    }, [savedRecordings]);
   const handleButtonClick = () => {
     document.getElementById("dropzone-file")?.click();
   };
   const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const selectedFile = event.target.files?.[0];
     if (selectedFile) {
-      console.log("Selected file:", selectedFile.name);
-      setFile(selectedFile);
+      const reader = new FileReader();
+      reader.onload = () => {
+        const base64Audio = reader.result as string;
+        const newRecording = {
+          name: selectedFile.name,
+          audio: base64Audio, 
+        };
+        setSavedRecordings((prev: any) => [...prev, newRecording]);
+      };
+      reader.readAsDataURL(selectedFile);
     }
   };
+ 
   const [isPlayingMap, setIsPlayingMap] = useState<{ [key: number]: boolean }>({});
   const [wavesurfers, setWavesurfers] = useState<any>({});
 
@@ -62,8 +82,9 @@ export default function Body() {
       wavesurfers[index].playPause();
     }
   };
-  
+
   const deleteItem = (index: number) => {
+    removeRecording(index);
     const updatedRecordings = savedRecordings.filter(
       (_: any, i: number) => i !== index
     );
@@ -71,7 +92,64 @@ export default function Body() {
   };
 
   const handleNewRecording = (recording: { name: string; audio: string }) => {
-    setSavedRecordings((prev: any) => [...prev, recording]);
+    const newRecording = {
+      ...recording,
+    };
+    setSavedRecordings((prev: any) => [...prev, newRecording]);
+  };
+
+  const handelConvert = async (index: number) => {
+    try {
+      const recording = savedRecordings[index];
+      if (!recording || !recording.audio) {
+        alert("فایلی برای ارسال یافت نشد.");
+        return;
+      }
+
+      // فعال کردن وضعیت "صبر کنید" برای دکمه مربوطه
+      setConverting((prev: any) => {
+        const updated = [...prev];
+        updated[index] = true;
+        return updated;
+      });
+
+      // تبدیل Base64 به Blob
+      const base64Data = recording.audio.split(",")[1]; // حذف prefix
+      const byteCharacters = atob(base64Data);
+      const byteNumbers = new Array(byteCharacters.length)
+        .fill(0)
+        .map((_, i) => byteCharacters.charCodeAt(i));
+      const byteArray = new Uint8Array(byteNumbers);
+      const audioBlob = new Blob([byteArray], { type: "audio/webm" });
+
+      // ایجاد FormData و ارسال درخواست
+      const formData = new FormData();
+      formData.append("file", audioBlob, `${recording.name}.webm`);
+      formData.append('model_type',selectedModel)
+      console.log(formData);
+
+      const response = await api.post("/api/enh/file", formData, {
+        headers: { "Content-Type": "multipart/form-data" },
+      });
+      toast.success("پردازش با موفقیت به اتمام رسید");
+      console.log("نتیجه سرور:", response.data);
+    
+      setSucsessFullConverting((prev: any) => {
+        const updated = [...prev];
+        updated[index] = true;
+        return updated;
+      });
+    } catch (err) {
+      console.error("خطا در ارسال فایل:", err);
+      toast.error("مشکلی پیش آمده لطفا دوباره تلاش کنید");
+    } finally {
+      // بازگرداندن وضعیت دکمه به حالت اولیه
+      setConverting((prev: any) => {
+        const updated = [...prev];
+        updated[index] = false;
+        return updated;
+      });
+    }
   };
   useEffect(() => {
     localStorage.setItem("Speech", JSON.stringify(savedRecordings));
@@ -79,22 +157,29 @@ export default function Body() {
 
 
   return (
-    <div className="bg-blue-50 max-h-screen h-auto flex font-Byekan  mt-20 justify-around">
+    <>
+     <div className="bg-blue-50 max-h-screen h-auto flex font-Byekan  mt-20 justify-around">
       <div className="extended-file">
-      {savedRecordings.length > 0 ? (
+        {savedRecordings.length > 0 ? (
           <>
             <div className="flex justify-end">
               <span className="text-gray-500 font-Byekan text-lg">
-              : فایل های موجود برای افزایش کیفیت
+                : فایل های موجود برای افزایش کیفیت
               </span>
             </div>
             <div className="border-b-2 border-gray-600">
             {savedRecordings.map(
-                (item: { name: string; audio: string }, index: number) => (
+                (
+                  item: { name: string; audio: string; language: string },
+                  index: number
+                ) => (
                   <div className="mt-2 mb-3" key={index}>
                     <span>{item.name}:</span>
                     <div className="flex items-center w-full border rounded-full px-3 mb-2">
-                      <button className="text-lg mx-2" onClick={() => onPlayPause(index)}>
+                      <button
+                        className="text-lg mx-2"
+                        onClick={() => onPlayPause(index)}
+                      >
                         {isPlayingMap[index] ? (
                           <span className="text-red-500 text-3xl">
                             <FaPauseCircle />
@@ -105,14 +190,24 @@ export default function Body() {
                           </span>
                         )}
                       </button>
-                      <div className="w-5/6 ">
+                      <div className="w-72 ">
                         <WavesurferPlayer
                           height={50}
                           waveColor="blue"
                           url={item.audio}
-                          onReady={(ws) => onReady(ws, index)}
-                          onPlay={() => setIsPlayingMap((prev) => ({ ...prev, [index]: true }))}
-                          onPause={() => setIsPlayingMap((prev) => ({ ...prev, [index]: false }))}
+                          onReady={(ws: any) => onReady(ws, index)}
+                          onPlay={() =>
+                            setIsPlayingMap((prev) => ({
+                              ...prev,
+                              [index]: true,
+                            }))
+                          }
+                          onPause={() =>
+                            setIsPlayingMap((prev: any) => ({
+                              ...prev,
+                              [index]: false,
+                            }))
+                          }
                         />
                       </div>
                     </div>
@@ -124,12 +219,23 @@ export default function Body() {
                       >
                         <MdDeleteForever />
                       </span>
-
-                      <span className="text-white text-base mx-2 bg-gradient-to-r px-16 py-2 cursor-pointer hover:scale-105 duration-200 rounded-2xl from-blue-600 to-blue-950">
-                        {" "}
-                        تبدیل
-                      </span>
+                      {converting[index] ? (
+                        <span className="text-white text-base mx-2 bg-gradient-to-r px-16 py-2 cursor-pointer hover:scale-105 duration-200 rounded-2xl from-blue-600 to-blue-950">
+                          {" "}
+                          <img src={loader} className="w-6 h-5" />
+                        </span>
+                      ) : !sucsessFullConverting[index] ? (
+                        <span
+                          onClick={() => handelConvert(index)}
+                          className="text-white text-base mx-2 bg-gradient-to-r px-16 py-2 cursor-pointer hover:scale-105 duration-200 rounded-2xl from-blue-600 to-blue-950"
+                        >
+                          {" "}
+                          شروع پردازش
+                        </span>
+                      ) :(<span>slam</span>)}
+                    
                     </div>
+                   
                   </div>
                 )
               )}
@@ -152,18 +258,18 @@ export default function Body() {
           </span>
         </div>
         <div className="mt-5 flex items-center" dir="rtl">
-          <span  onClick={() => setIsOpen(!isOpen)} className="text-gray-400 flex items-center cursor-pointer text-lg bg-white rounded-lg px-6 py-3 ">{selectedLanguages}
-          <span className="mr-3"><CiSquareChevDown/></span>
+          <span onClick={() => setIsOpen(!isOpen)} className="text-gray-400 flex items-center cursor-pointer text-lg bg-white rounded-lg px-6 py-3 ">{selectedModel}
+            <span className="mr-3"><CiSquareChevDown /></span>
           </span>
-         
+
         </div>
         {isOpen && (
           <div className="flex mt-2  w-40 items-center z-50 flex-col origin-top-right absolute py-5 px-2 bg-white text-gray-700 rounded-xl text-base">
-            {listLanguage.map((item, i) => (
+            {listModel.map((item, i) => (
               <div
                 key={i}
                 onClick={() => {
-                  setSelectedLanguages(item);
+                  setSelectedModel(item);
                   setIsOpen(false);
                 }}
                 className="hover:bg-gray-200 justify-center hover:text-blue-600 py-3 px-5 border-b-2 w-full cursor-pointer  flex text-center items-center"
@@ -175,7 +281,7 @@ export default function Body() {
         )}
       </div>
 
-      <div className="input-div ">
+      <div className="input-div  border border-dashed border-gray-800 rounded-md p-10 max-h-[60vh] justify-center ">
         <input
           id="dropzone-file"
           type="file"
@@ -183,13 +289,18 @@ export default function Body() {
           className="hidden"
           onChange={handleFileChange}
         />
-        <div className="flex justify-end mb-5">
+        <div className="flex justify-center items-center">
+          <span className="text-9xl text-blue-600">
+            <FaCloudUploadAlt />
+          </span>
+        </div>
+        <div className="flex justify-center mb-5">
           <span className="text-gray-500 font-Byekan font-bold text-lg">
             : انتخاب فایل از سیستم
           </span>
         </div>
 
-        <div className="mb-16 flex" dir="rtl">
+        <div className="mb-16 flex justify-center" dir="rtl">
           <button
             onClick={handleButtonClick}
             className="flex items-center px-6 py-2 bg-gradient-to-r from-blue-600 to-blue-950 opacity-80 rounded-xl font-black text-xl shadow-2xl hover:opacity-100 border-[3px] border-blue-200 text-white"
@@ -209,8 +320,12 @@ export default function Body() {
           )}
         </div>
 
-        <VoiceRecorder nameComponent="Speech"  onRecordingComplete={handleNewRecording}/>
+        <VoiceRecorder nameComponent="Speech" onRecordingComplete={handleNewRecording} />
       </div>
+      
     </div>
+    <ToastContainer position="bottom-right" />
+    </>
+   
   )
 }
